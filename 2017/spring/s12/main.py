a139de782f30dc8fad7333875232b788095fbe7b
Iman Tabrizian <Tabrizian@outlook.com>
2017-05-15 10:12:25 +0430

    Improves the sample code

--- 2017/spring/s12/main.py
+++ 2017/spring/s12/main.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 
 import sys
-import time
-from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QGridLayout, QLabel
+from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QGridLayout
 # from PyQt5.QtCore import QCoreApplication
-x = input('write your height :')
-y = input('write your weight :')
+x = input('Enter the height of table: ')
+y = input('Enter the width of table: ')
+
 
 class Example(QWidget):
 
@@ -15,23 +14,29 @@ class Example(QWidget):
         self.init_ui()
 
     def init_ui(self):
+        # Create a grid layout
         layout = QGridLayout()
 
-        for i in range(1, int(x)+1):
-            for j in range(1, int(y)+1):
+        # Add all of the buttons to the grid layout
+        for i in range(1, int(x) + 1):
+            for j in range(1, int(y) + 1):
                 newlabel = QPushButton()
+                # Change the color to green
                 newlabel.setStyleSheet('background-color: green')
+                # Set the action when the button was clicked to im_clicked
                 newlabel.clicked.connect(self.im_clicked)
                 layout.addWidget(newlabel, i, j)
 
         self.layout = layout
         self.setLayout(layout)
+        # Initial Size of the screen
         self.setGeometry(30, 40, 400, 400)
         self.setWindowTitle('Quit button')
         self.show()
 
     def im_clicked(self):
         sender = self.sender()
+        # Change the color to yellow
         sender.setStyleSheet('background-color: yellow')
 
 if __name__ == '__main__':
@@ -40,4 +45,3 @@ if __name__ == '__main__':
     ex = Example()
 
     sys.exit(app.exec_())
-    time.sleep(2)
